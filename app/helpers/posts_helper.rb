16d2e6bba713347b1abe5be0cc4d4d88294c32cc
teckjon <demospata@hotmail.com>
2015-09-15 17:28:49 +0000

    Checkpoint 31 assignment

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,10 @@
 module PostsHelper
+    
+    def render_censored_title(post)
+        output = post.title
+        if post.id % 5 == 0 
+           output = "<strong>#{post.censored_title}</strong>"
+       end
+       output.html_safe
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
     has_many :comments
+    
+    def censored_title
+        self.id % 5 == 0 ? "CENSORED" : self.title
+    end
 end
