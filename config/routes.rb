8b739de6b34b11efe1808a933f805f522de70797
teckjon <demospata@hotmail.com>
2015-09-17 03:08:37 +0000

    Assignment 32 Incomplete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+ resources :advertisements
+ 
  resources :posts
  
  get 'welcome/faq'
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,95 +2,66 @@ require 'rails_helper'
 include RandomData
 
 RSpec.describe PostsController, type: :controller do
-  let (:my_post) { Post.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
-  
+  let (:my_ad) { Advertisement.create(title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 100) }
+
   describe "GET #index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
-    
-    it "assigns [my_post] to @posts" do
-       get :index
-       expect(assigns(:posts)).to eq([my_post])
-    end   
+
+    it "assigns my_ad to @advertisements" do
+      get :index
+      expect(assigns(:advertisements)).to eq([my_ad])
+    end
   end
-  
+
+  describe "GET show" do
+    it "returns http success" do
+      get :show, {id: my_ad.id}
+      expect(response).to have_http_status(:success)
+    end
+    it "renders the #show view" do
+      get :show, {id: my_ad.id}
+      expect(response).to render_template :show
+    end
+
+    it "assigns my_ad to @advertisement" do
+      get :show, {id: my_ad.id}
+      expect(assigns(:advertisement)).to eq(my_ad)
+    end
+  end
+
   describe "GET new" do
-      it "returns http success" do
-        get :new
-        expect(response).to have_http_status(:success)
-      end
- 
- # #2
-      it "renders the #new view" do
-        get :new
-        expect(response).to render_template :new
-      end
- 
- # #3
-      it "instantiates @post" do
-        get :new
-        expect(assigns(:post)).not_to be_nil
-      end
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
     end
- 
-    describe "POST create" do
- # #4
-      it "increases the number of Post by 1" do
-        expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
-      end
- 
- # #5
-      it "assigns the new post to @post" do
-        post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
-        expect(assigns(:post)).to eq Post.last
-      end
- 
- # #6
-      it "redirects to the new post" do
-        post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
-        expect(response).to redirect_to Post.last
-      end
+
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
     end
-     
-     def new
- # #7
-   @post = Post.new
-     end
-   
-   describe "GET show" do
-     it "returns http success" do
- # #16
-       get :show, {id: my_post.id}
-       expect(response).to have_http_status(:success)
-     end
-     it "renders the #show view" do
- # #17
-       get :show, {id: my_post.id}
-       expect(response).to render_template :show
-     end
- 
-     it "assigns my_post to @post" do
-       get :show, {id: my_post.id}
- # #18
-       expect(assigns(:post)).to eq(my_post)
-     end
-   end
 
+    it "instantiates @advertisement" do
+      get :new
+      expect(assigns(:advertisement)).not_to be_nil
+    end
+  end
 
- # describe "GET new" do
- #   it "returns http success" do
- #     get :new
- #     expect(response).to have_http_status(:success)
- #   end
- # end
+  describe "POST create" do
+    it "increases the number of Advertisement by 1" do
+      expect{post :create, advertisement: {title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 100}}.to change(Advertisement,:count).by(1)
+    end
 
- #  describe "GET edit" do
- #    it "returns http success" do
- #      get :edit
- #      expect(response).to have_http_status(:success)
- #    end
- #  end
+    it "assigns the new post to @advertisement" do
+      post :create, advertisement: {title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 100}
+      expect(assigns(:advertisement)).to eq Advertisement.last
+    end
 
+    it "redirects to the new advertisement" do
+      post :create, advertisement: {title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 100}
+      expect(response).to redirect_to Advertisement.last
+    end
+  end
 end
