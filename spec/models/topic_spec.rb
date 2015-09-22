b6773c30de8b2b06323a976998d8a60c67818b92
teckjon <demospata@hotmail.com>
2015-09-22 16:20:37 +0000

    Assignment 35 completed

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -5,6 +5,8 @@
    let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
 
    it { should have_many(:posts) }
+   it { should validate_length_of(:title).is_at_least(5) }
+   it { should validate_length_of(:description).is_at_least(15) }      
  
  # #1
    describe "attributes" do
