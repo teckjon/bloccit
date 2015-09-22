bafb9476d39be0457061bc363030782ecfd62790
teckjon <demospata@hotmail.com>
2015-09-22 04:37:06 +0000

    Checkpoint 35 Validation completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ActiveRecord::Base
     belongs_to :topic
    
     has_many :comments, dependent: :destroy
+   
+    validates :title, length: { minimum: 5 }, presence: true
+    validates :body, length: { minimum: 20 }, presence: true
+    validates :topic, presence: true    
     
     def censored_title
         self.id % 5 == 0 ? "CENSORED" : self.title
