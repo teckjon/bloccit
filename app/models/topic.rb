86996a941036d2b200b6dbd2b337119414b22136
teckjon <demospata@hotmail.com>
2015-10-05 19:52:40 +0000

    Checkpoint 49 completed, private

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,4 +3,6 @@ class Topic < ActiveRecord::Base
     has_many :labelings, as: :labelable
  # #14
     has_many :labels, through: :labelings    
+   
+    scope :visible_to, -> user { user ? all : where(public: true) } 
 end
