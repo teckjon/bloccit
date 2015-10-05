ecc58c9be51076f3ebb9ac070b97ba5351cc2762
teckjon <demospata@hotmail.com>
2015-10-05 18:53:20 +0000

    Assignment-48-Public

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,5 +6,32 @@ module ApplicationHelper
      else
        content_tag :div, capture(&block), class: 'form-group'
      end
-   end    
+   end 
+   
+   def render_posts_title(posts)
+       if posts.any? 
+        output =  "<h2>Posts</h2>"
+       else
+        output = "<p>This user has not submitted any posts yet</p>"
+       end
+       output.html_safe 
+   end
+   
+   def render_comments_title(comments)
+       if comments.any?
+           coutput = "<h2>Comments</h2>"
+       else
+           coutput = "<p>This user has not submitted any comments yet</p>"
+       end
+       coutput.html_safe
+   end       
+   
+   def render_favorite_title(favorites)
+       if favorites.any?
+           foutput = "<h2>Favorites</h2>"
+       else 
+           foutput = "<p>This user does not have any favorites</p>"
+       end
+       foutput.html_safe
+   end   
 end
